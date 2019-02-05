8c5bc676174b920d494d33b451bda6003160eb2d
Jason Robert <jason@grandcircus.co>
2019-02-04 21:24:47 -0500

    fifty two

---
+++ Ahbc.Class.FiftyTwo/Ahbc.Class.FiftyTwo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ahbc.Class.FiftyTwo.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var claimsPrincipal = User as ClaimsPrincipal;
+            var surnameClaim = claimsPrincipal.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Surname);
+
+            ViewBag.Message = $"Hello {surnameClaim.Value}";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Ahbc.Class.FiftyTwo/Ahbc.Class.FiftyTwo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ahbc.Class.FiftyTwo.Startup))]
+namespace Ahbc.Class.FiftyTwo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
